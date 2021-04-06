cd5d4fe9c59961b50dfa3f61efd1afc50510e15c
Carlos <cviche@ucsc.edu>
2021-04-05 21:56:06 -0700

    Finished navbar

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,18 +1,39 @@
 import React, { Fragment } from "react";
 import "./Login.scss";
+// import
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  handleSignIn = () => {
+    console.log("Hello world");
+  };
+
   render() {
     return (
       <>
         <header className="header">
           <nav className="nav">
             <ul className="ul">
-              <li className="li">Hello world</li>
-              <li className="li">Hello world</li>
+              <li className="li">
+                <button onClick={this.handleSignIn} className="logo-text">
+                  NeedToDo
+                </button>
+              </li>
+              <li className="li">
+                <button onClick={this.handleSignIn} className="sign-in">
+                  Sign In
+                </button>
+              </li>
             </ul>
           </nav>
         </header>
+        <div class="main-body">
+          <img src="images/checklist.png" alt="checklist" />
+        </div>
       </>
     );
   }
