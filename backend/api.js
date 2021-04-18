c5dbc2873c82b863b98e7f24fe2caf52265469c8
Carlos <cviche@ucsc.edu>
2021-04-18 11:58:43 -0700

    Trying to find out why req.body is empty

--- backend/api.js
+++ backend/api.js
@@ -1,3 +1,15 @@
+const bcrypt = require("bcrypt");
+
 exports.login = async (req, res) => {
-  res.send("You are logged in!");
+  console.log("SERVER: Trying to login a user");
+  try {
+    const user_pw = req.body;
+    console.log(user_pw);
+    // const salt = await bcrypt.genSalt();
+    // const hashed_pw = bcrypt.hash(user_pw, salt);
+    res.send("You are logged in!");
+  } catch (error) {
+    console.log(error);
+    res.send("An error has occured");
+  }
 };
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,13 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
-require("dotenv").config();
 const frontend = process.env.FRONTEND_SERVER;
 const port = process.env.PORT || 4000;
 const cors = require("cors");
 const api = require("./api");
-
 app.use(cors({ origin: frontend }));
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
