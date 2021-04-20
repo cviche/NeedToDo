eb57af9d7245d229ec86faa203883cc0ee370f95
Carlos <cviche@ucsc.edu>
2021-04-19 21:52:42 -0700

    Added home page component boiler plate

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,18 @@
+import React, { Fragment } from "react";
+
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <div>Hello world</div>
+      </Fragment>
+    );
+  }
+}
+
+export default Home;
