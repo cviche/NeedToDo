06cc826cd1b80bfba227b2e16b0dd706b66caddc
cviche <60164780+cviche@users.noreply.github.com>
2021-04-24 18:35:04 -0700

    Working on modal when adding a new task

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,23 +1,56 @@
 import React, { Fragment } from "react";
 import "./Home.scss";
+import { authenticate } from "../api_calls";
+import backend_host from "../host";
 
 class Home extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      notes: [{}],
+    };
   }
 
-  componentDidMount() {
+  // Redirects to login page if they are not authenticated.
+  componentDidMount = async () => {
     document.body.style.background = "#DAECFC";
+    const auth_successful = await authenticate(backend_host);
+    if (auth_successful === false) {
+      console.log("We are pushing the user to the home page");
+      this.props.history.push("/login");
+    }
+    return;
+  };
 
-    //Authenticate user
-  }
-
-  handleSignOut() {}
+  handleSignOut = (event) => {
+    try {
+      localStorage.removeItem("token");
+      this.props.history.push("/login");
+    } catch (error) {
+      console.log(error);
+      console.log(
+        "There was an error when trying to sign out. Please try again"
+      );
+    }
+  };
 
   render() {
     return (
       <Fragment>
+        <div id="myModal" class="modal">
+          <div class="modal-content">
+            <span class="close">&times;</span>
+            {/* <input type="text" placeholder="Enter a task" /> */}
+            <textarea
+              name="new_task"
+              id="task"
+              cols="30"
+              rows="10"
+              placeholder="Enter a new task"
+            ></textarea>
+            <button className="add-task-button">Add task</button>
+          </div>
+        </div>
         <header>
           <nav className="home-nav">
             <div></div>
@@ -37,6 +70,11 @@ class Home extends React.Component {
         </header>
         <section className="notes-section">
           <p>
+            {[1] == false ? (
+              <div>Empty array is false</div>
+            ) : (
+              <div>Empty array is true</div>
+            )}
             You have currently have no tasks. Click the “+” icon above to add a
             task
           </p>
